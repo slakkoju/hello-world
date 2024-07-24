7f281bc8b44c7153b60a9b6dac8888c7b7bd5176
Satish Lakkoju <33646121+slakkoju@users.noreply.github.com>
2024-07-24 14:38:43 -0400

    test code scanning

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,5 +1,13 @@
+import re
 def say_hello():
     return "Hello, World!"
+
+def filterScriptTags(content): 
+    oldContent = ""
+    while oldContent != content:
+        oldContent = content
+        content = re.sub(r'<script.*?>.*?</script>', '', content, flags= re.DOTALL | re.IGNORECASE)
+    return content
 
 if __name__ == "__main__":
     print(say_hello())

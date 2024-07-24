1a740e8eaa0c4d9a4781acac271f9a6904c11b14
Satish Lakkoju <slakkoju@ebsco.com>
2024-07-24 12:43:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# platform.training.github-actions-flask-api
+create application with github actions
+# hello-world
---
+++ tests/unit/test_hello.py
@@ -0,0 +1,4 @@
+from hello_world.hello import say_hello
+
+def test_say_hello():
+    assert say_hello() == "Hello, World!"
